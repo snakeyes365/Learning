4032340895cdbeead4191787945f1bea00f75016
snakeyes365 <niteshjaiswal2014@gmail.com>
2019-04-10 12:01:27 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,5 +2,41 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function buttonClick()
+   {
+    var x=document.getElementById("text1").value;
+    alert("Entered text is: "+x); 
+   }
+   function buttonClick2()
+   {
+      if(rd1.checked==true)
+      alert("Selected car brand is : "+rd1.value);
+      else if(rd2.checked==true)
+      alert("Selected car brand is : "+rd2.value);
+      else
+      alert("No option Selected");
+   }
+
+     function buttonClick3()
+     {
+       var x=document.getElementById("selectbox");
+       alert(x.options[x.selectedIndex].value);
+     }
+
+       function validate()
+       {
+         var uname=document.getElementById("textbox2");
+         var pass=document.getElementById("textbox3");
+         if(uname.value==""||pass.value=="")
+         {
+           alert("No blank Values Allowed");
+           return false;
+         }
+         else if(pass.value<5)
+         {
+         alert("password is too short");
+         return false;
+         }
+         else
+         true;
+       }
